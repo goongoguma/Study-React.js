6ddd69712d7b9cee5763dc2ded2cb40c94d2695c
jaehyun <wogus7an@gmail.com>
2020-12-29 21:37:47 +0900

    hoc loading component

---
+++ src/05/withLoading.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function (loadingMessage = '로딩 중') {
+  return function withLoading(WrappedComponent) {
+    const { displayName, name: componentName } = WrappedComponent;
+    const wrappedComponentName = displayName || componentName;
+
+    function WithLoading({ isLoading }) {
+      if (isLoading) return loadingMessage;
+
+      return (
+        <WrappedComponent {...props} />
+      );
+    };
+    WithLoading.displayName = `withLoading(${wrappedComponentName})`;
+    return WithLoading;
+  }
+}
